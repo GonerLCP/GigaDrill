61ca16f416cb8b5ca9bec76b35adb240874efc35
GonerLCP <antonin.arnold54700@gmail.com>
2025-06-07 14:20:46 -0400

    DebutDriller

--- Assets/Scripts/Manager/RythmManager.cs
+++ Assets/Scripts/Manager/RythmManager.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
 
+public enum Bouton
+{
+    Y,B,A,X
+}
 public class RythmManager : MonoBehaviour
 {
     public static RythmManager Instance { get; private set; }
     public bool RythmWindow;
+    public Bouton btn;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,5 +27,10 @@ public class RythmManager : MonoBehaviour
         print(RythmWindow);
         RythmWindow = !RythmWindow; //Attention, peut créer des problèmes si on exit l'anim pas au bon moment à préter attention
         //RythmWindow = RythmWindow ? false : true;
+    }
+
+    public void ChangeButton()
+    {
+        //btn = Bouton.;
     }
 }
