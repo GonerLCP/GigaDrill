1552394ca2565c7a4fe847e9bd1599b8e1ff1e85
GonerLCP <antonin.arnold54700@gmail.com>
2025-06-06 23:26:58 -0400

    FinPrototypedétruire

--- Assets/Scripts/PlayerTileDestroyer.cs
+++ Assets/Scripts/PlayerTileDestroyer.cs
@@ -7,6 +7,17 @@ public class PlayerTileDestroyer : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.GetComponent<Player>().drilling == true)
+        {
+            Vector3 hitWorldPos = transform.position;
+            Vector3Int cellPos = targetTilemap.WorldToCell(hitWorldPos);
+
+            if (targetTilemap.HasTile(cellPos))
+            {
+                targetTilemap.SetTile(cellPos, null); // Supprime la tile
+                Debug.Log("Tile supprimée à " + cellPos);
+            }
+        }
         // Vérifie que le collider appartient bien à la tilemap (ou à un layer/tag particulier si tu préfères)
         if (other.GetComponent<TilemapCollider2D>())
         {
